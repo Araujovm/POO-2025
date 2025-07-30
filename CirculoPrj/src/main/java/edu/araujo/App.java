dc12fc3d28dad2fe86138005b7369169b39699f6
Araujovm <felipearaujovm564@gmail.com>
2025-07-30 14:09:00 +0000

    configuracoes do ambiente

---
+++ CirculoPrj/src/main/java/edu/araujo/App.java
@@ -0,0 +1,7 @@
+package edu.araujo;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, world! Projeto CirculoPrj criado com sucesso.");
+    }
+}
---
+++ CirculoPrj/src/main/java/edu/araujo/Circulo.java
@@ -0,0 +1,8 @@
+package edu.araujo;
+
+public class Circulo{
+
+    public Circulo(){
+
+    }
+}
