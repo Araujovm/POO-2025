6c9b1f35bfb8eb18c4a0e34329a16461c0ead60c
Araujovm <felipearaujovm564@gmail.com>
2025-08-04 14:10:02 +0000

    dia 4/8

--- CirculoPrj/src/main/java/edu/araujo/App.java
+++ CirculoPrj/src/main/java/edu/araujo/App.java
@@ -2,6 +2,12 @@ package edu.araujo;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, world! Projeto CirculoPrj criado com sucesso.");
+        Circulo c1 = new Circulo();
+        System.out.println(c1.toString());
+        c1.zoom(4);
+        c1.mover(4, 2);
+        System.out.println(c1.toString());
+        System.out.println("area do circulo"+c1.area());
     }
 }
+
--- CirculoPrj/src/main/java/edu/araujo/Circulo.java
+++ CirculoPrj/src/main/java/edu/araujo/Circulo.java
@@ -1,8 +1,36 @@
 package edu.araujo;
 
+
 public class Circulo{
+    private double cordX;
+    private double cordY;
+    private double raio;
 
     public Circulo(){
+        this.cordX = 0;
+        this.cordY = 0;
+        this.raio = 1;
+    }
 
+    public Circulo(double umX, double umY,double umRaio){
+        this.cordX = umX;
+        this.cordY = umY;
+        this.raio = umRaio;
+    }
+
+    public void mover(double novoX, double novoY){
+        this.cordX = novoX;
+        this.cordY = novoY;
+    }
+
+    public void zoom(double fator){
+        this.raio = fator *raio;
+    }
+
+    public double area(){
+       return Math.PI*Math.pow(raio,2);
+    }
+    public String toString(){
+        return "Circulo de raio "+raio+" posição ("+cordX+","+cordY+")";
     }
 }
